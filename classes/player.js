3937f23a2e58e6fca6383e547f0956bd8e0047ed
Théo Bernard <theo.bernard@moonshot-internet.com>
2021-06-14 12:29:03 +0200

    Setup Player & Weapon classes

---
+++ classes/player.js
@@ -0,0 +1,10 @@
+class Player {
+    constructor() {
+        this.id = id;
+        this.color;
+        this.health = 100;
+        this.mobility = 3;
+    }
+}
+
+export default Player;
---
+++ classes/weapon.js
@@ -0,0 +1,10 @@
+class Weapon {
+    constructor() {
+        this.weaponId = weaponId;
+        this.weaponName = weaponName;
+        this.power = weaponPower;
+        this.criticalRate = criticalRate;
+    }
+}
+
+export default Weapon;
