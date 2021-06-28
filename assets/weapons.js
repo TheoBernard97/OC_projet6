8a85b8de3516e2fb77efec344957996797d4c48a
Théo Bernard <theo.bernard@moonshot-internet.com>
2021-06-28 22:13:55 +0200

    Update weapons damages

--- assets/weapons.js
+++ assets/weapons.js
@@ -2,28 +2,28 @@ const Weapons = [
    {
       "weaponId": 1,
       "weaponName": "sword1",
-      "weaponPower": 4,
+      "weaponPower": 10,
       "criticalRate": 45,
       "effect": null
    },
    {
       "weaponId": 2,
       "weaponName": "sword2",
-      "weaponPower": 6,
+      "weaponPower": 15,
       "criticalRate": 30,
       "effect": null
    },
    {
       "weaponId": 3,
       "weaponName": "sword3",
-      "weaponPower": 8,
+      "weaponPower": 20,
       "criticalRate": 15,
       "effect": null
    },
    {
       "weaponId": 4,
       "weaponName": "sword4",
-      "weaponPower": 10,
+      "weaponPower": 30,
       "criticalRate": 1,
       "effect": null
    }
