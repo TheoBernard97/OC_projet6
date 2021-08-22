11d37eb310986cfda8d518c6c3d719e122688338
Théo Bernard <theo.bernard@moonshot-internet.com>
2021-08-22 18:51:40 +0200

    🔖  Balance patch 1.0.1

--- assets/weapons.js
+++ assets/weapons.js
@@ -1,32 +1,34 @@
+/** @format */
+
 const Weapons = [
-   {
-      "weaponId": 1,
-      "weaponName": "sword1",
-      "weaponPower": 10,
-      "criticalRate": 45,
-      "effect": null
-   },
-   {
-      "weaponId": 2,
-      "weaponName": "sword2",
-      "weaponPower": 15,
-      "criticalRate": 30,
-      "effect": null
-   },
-   {
-      "weaponId": 3,
-      "weaponName": "sword3",
-      "weaponPower": 20,
-      "criticalRate": 15,
-      "effect": null
-   },
-   {
-      "weaponId": 4,
-      "weaponName": "sword4",
-      "weaponPower": 30,
-      "criticalRate": 1,
-      "effect": null
-   }
-]
+  {
+    weaponId: 1,
+    weaponName: "sword1",
+    weaponPower: 15,
+    criticalRate: 45,
+    effect: null,
+  },
+  {
+    weaponId: 2,
+    weaponName: "sword2",
+    weaponPower: 20,
+    criticalRate: 30,
+    effect: null,
+  },
+  {
+    weaponId: 3,
+    weaponName: "sword3",
+    weaponPower: 25,
+    criticalRate: 15,
+    effect: null,
+  },
+  {
+    weaponId: 4,
+    weaponName: "sword4",
+    weaponPower: 30,
+    criticalRate: 5,
+    effect: null,
+  },
+];
 
 export default Weapons;
