237de6ba4263651cec15b8dcd775d84e458dad71
Théo Bernard <theo.bernard.94100@gmail.com>
2021-11-13 15:44:48 +0100

    ♻️ Setup & use jQuery

--- app.js
+++ app.js
@@ -1,12 +1,11 @@
 /** @format */
 
 import Game from "./classes/game.js";
-
 let game = new Game();
 
-const start = document.querySelector(".start-btn");
-start.addEventListener("click", () => {
-  start.style.display = "none";
+const start = $(".start-btn");
+start.click(() => {
+  start.hide();
   game.init();
 });
 
